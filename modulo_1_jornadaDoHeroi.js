d1e2927281288d6a7ff4b41868e6e81afd1d31ea
Dener Gomes Batista <100161372+denerbatista@users.noreply.github.com>
2022-03-10 01:13:52 -0300

    Update modulo_1_jornadaDoHeroi.js
    Projeto 1 - A Jornada do Herói
    Hoje nós começamos o nosso primeiro projeto entregável! Esse projeto vai avaliar os conteúdos passados durante essa primeira semana, e se você consegue desenvolver uma programação utilizando todos eles.
    Você poderá usar as aulas de Codelab para trabalhar no seu projeto.
    A premissa é a seguinte: O nosso herói chegou ao fim da sua jornada, ao desafio final, e você pode criar um background legal resumindo qual foi essa jornada e onde ele chegou. Use sua criatividade! Vale qualquer coisa: Uma guerreira que percorreu um longo caminho para enfrentar um bruxo e salvar seu povo, um astronauta que teve que viajar aos confins da galáxia para deter uma anomalia que iria engolir seu planeta, ou uma pessoa que está voltando para casa ao fim de um dia cansativo e precisa fazer o jantar.
    Dependendo de como foi sua jornada, dos objetivos que foram concluídos no caminho, teremos um resultado final diferente.
    Requisitos
    Você deve criar um programa que faça 5 perguntas para o usuário, e dependendo das respostas, vai exibir um resultado diferente no final. As perguntas devem ser todas de Sim ou Não, e devem ser referentes aos objetivos que ele tiveram que ser cumpridos durante a jornada, por exemplo: "Você conseguiu encontrar a Espada Mágica da Chama da Estrela do Inverno?", "Você conseguiu consertar os reatores nucleares do motor de dobra?", "Você se lembrou de passar no mercado e comprar óleo?"
    No final, o programa deve fazer uma contagem de quantas respostas foram Sim, e exibir o resultado de acordo com esse número:
    0 respostas Sim: Você falha miseravelmente.
    1 ou 2 respostas Sim: Você falha, mas ainda consegue fugir da situação.
    3 respostas Sim: Você chega perto de conseguir alcançar seu objetivo, mas acaba falhando por pouco.
    4 respostas Sim: Depois de muito esforço você conquista seu objetivo, embora não de maneira perfeita.
    5 respostas Sim: Você triunfa de maneira inquestionável e seus feitos serão lembrados por muitas gerações.
    O programa deve contar a história inicial (background), fazer as perguntas, e ao final, exibir o resultado de acordo com as situações acima.

--- modulo_1_jornadaDoHeroi.js
+++ modulo_1_jornadaDoHeroi.js
@@ -3,10 +3,10 @@ const prompt=require('prompt-sync')()
 let resp
 let respS=[]
 const nome=prompt("Digite seu nome Herói: ")
-console.clear()
 const pergs=[`Primeiramente seja grato e fiel separou seu dizimo e oferta ${nome}? `,`Pagou água e luz ${nome}?, você precisa de água e energia, principalmente pra um banho quente antes do rolê. `,`Diga com sinceridade ${nome} pagou IPTU desse ano? `,`Não gaste tudo com besteiras ${nome}, você separou a grana do mercado? `,`E o cartão de crédito ${nome}, vc sabe a facada que são os juros, pagou ? `]
 const result=[`Vish desse jeito ${nome}, você não pode botar a cara na rua acerta isso pra ficar de boa`,`Isso ainda é pouco,tem que agilizar isso ${nome}, seje mais responsável `,`Isso ainda é pouco,tem que agilizar isso ${nome}, seje mais responsável `,`Não está totalmente ruim mas o essencial o próprio nome já diz não é mesmo ${nome}.`,`Vamos lá não perde o pique, só falta mais uma, não deixe pra depois ${nome}.`,`Exelente ${nome}, você mais que meresse essa Pizza não perca tempo e corra pra recompensa, só não gaste tudo guarde um pouquinho para o próximo.`]
 do{
+    console.clear()
     console.log(`\nFinal de mês chegando, vamos conferir se sobra uma graninha pra uma pizza ${nome} ? mas primeiro as contas essencias devem ser pagas, digite sim para confirmar ou qualquer tecla ou palavra para não.\n`)
     for(i=0,nivel=0;i<pergs.length;i++){
         resp=prompt(pergs[i]);
