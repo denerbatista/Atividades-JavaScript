13a1a311a49a5448a9dd1802711cd5747d74f919
Dener Gomes Batista <100161372+denerbatista@users.noreply.github.com>
2022-03-13 01:23:58 -0300

    Update modulo_1_jornadaDoHeroi.js
    Esta versão mantém o terminal limpo e organizado, faz cinco perguntas personalizadas com o nome do usuário, mostra a resposta anterior com identificação da pergunta, recebe apenas sim, s, nao e n, mas modifica para sim e não no array, da retorno de resposta inválida e repete pergunta, no fim classifica o resultado de acordo com as respostas sim demonstrando quantas foram, pergunta se deseja finalizar, ver a lista de perguntas com as respostas do usuário e/ou reiniciar a jornada.

--- modulo_1_jornadaDoHeroi.js
+++ modulo_1_jornadaDoHeroi.js
@@ -1,33 +1,40 @@
-console.clear()
 const prompt=require('prompt-sync')()
+let loop
 do{
     console.clear()
     let resp
     let respS=[]
     const nome=prompt("Digite seu nome Herói: ")
     const pergs=[`Primeiramente seja grato e fiél, separou seu dizimo e oferta ${nome}? `,`Pagou água e luz ${nome}?, você precisa de água e energia, principalmente pra um banho quente antes do rolê. `,`Diga com sinceridade ${nome} pagou IPTU desse ano? `,`Não gaste tudo com besteiras ${nome}, você separou a grana do mercado? `,`E o cartão de crédito ${nome}, você sabe a facada que são os juros, pagou ? `]
-    const result=[`Vish desse jeito ${nome}, você não pode botar a cara na rua acerta isso pra ficar de boa`,`Isso ainda é pouco,tem que agilizar isso ${nome}, seja mais responsável `,`Isso ainda é pouco,tem que agilizar isso ${nome}, seja mais responsável `,`Não está totalmente ruim você fez o mínimo esperado ${nome}.`,`Vamos lá não perde o pique, só falta mais uma, não deixe pra depois ${nome}.`,`Exelente ${nome}, você mais que meresse essa Pizza não perca tempo e corra pra recompensa, só não gaste tudo, guarde um pouquinho para o próximo fim de semana.`]
+    const result=[`Vish desse jeito ${nome}, você não pode botar a cara na rua acerta isso pra ficar de boa,`,`Isso ainda é pouco,tem que agilizar isso ${nome}, seja mais responsável,`,`Isso ainda é pouco,tem que agilizar isso ${nome}, seja mais responsável,`,`Não está totalmente ruim você fez o mínimo esperado ${nome},`,`Vamos lá não perde o pique, só falta mais uma, não deixe pra depois ${nome},`,`Exelente ${nome}, você mais que meresse essa Pizza não perca tempo e corra pra recompensa, \nsó não gaste tudo, guarde um pouquinho para o próximo fim de semana,`]
     console.clear()
-    console.log(`Grande ${nome}! Vamos conferir se todas as contas do mês foram pagas, para ir na pizzaria com a conciência limpa? Digite 'sim' ou 'nao' para as perguntas a seguir:\n`)
+    console.log(`Grande ${nome}! Vamos conferir se todas as contas do mês foram pagas, para ir na pizzaria com a conciência limpa? \nDigite 'sim' ou 'nao':\n`)
     for(i=0,nivel=0;i<pergs.length;i++){
         resp=prompt(pergs[i]);
         respS.push(resp.toLowerCase());
-        if(respS[i]==='sim'||respS[i]==='s'){
+        if(respS[i]=='sim'||respS[i]=='s'){
             nivel++
             console.clear()
-            console.log(`Sua resposta foi sim.\n`)
-        }else if(respS[i]==='nao'|respS[i]==='n'){
+            console.log(`Sua resposta da pergunta ${i+1} foi sim.\n`)
+            respS[i]='sim'
+        }else if(respS[i]=='nao'||respS[i]=='n'){
             console.clear()
-            console.log(`Sua resposta foi não.\n`)
+            console.log(`Sua resposta da pergunta ${i+1} foi não.\n`)
+            respS[i]='não'
         }else{
             console.clear()
-            console.log(`A resposta ${respS[i]} é inválida, responda sim ou nao\n`)
-            respS.splice(i, 1)
+            console.log(`A resposta ${respS[i]} é inválida, responda sim ou nao.\n`)
+            respS.length=i
             i=i-1
-            continue;
         }
     }
     console.clear()
-    console.log(`${result[nivel]} Você pagou ${nivel} contas\n`)
-    loop=prompt('Digite r para reiniciar ou qualquer tecla para finalizar: ').toLowerCase()
+    console.log(`${result[nivel]} você pagou ${nivel} contas.\n`)
+    loop=prompt('Digite "l" para listar todas as suas respostas, "r" para reiniciar ou qualquer tecla para finalizar: ').toLowerCase()
+    while(loop=='l'){
+        console.clear()
+        console.log(`${result[nivel]} você pagou ${nivel} contas.\n\n${pergs[0]}\nResposta: ${respS[0]}\n\n${pergs[1]}\nResposta: ${respS[1]}\n\n${pergs[2]}\nResposta: ${respS[2]}\n\n${pergs[3]}\nResposta: ${respS[3]}\n\n${pergs[4]}\nResposta: ${respS[4]}\n\n`)
+        loop=prompt('Digite r para reiniciar ou qualquer tecla para finalizar: ').toLowerCase()
+    }
+    console.clear()
 }while(loop=='r')
