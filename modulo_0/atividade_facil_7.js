f57b4245999f8e1cadd6fda5d55fb41b9604e29b
Dener Gomes Batista <denerbatista@icloud.com>
2022-03-16 19:14:52 -0300

    Diminuição de if e else do projeto 2 e correção ortografica

--- modulo_0/atividade_facil_7.js
+++
@@ -1,6 +0,0 @@
-const prompt = require('prompt-sync')();
-const data=new Date();
-let anoA=data.getFullYear();
-console.log("\nQual seu ano de nascimento ? ")
-let anoN=prompt()
-console.log(`\nPossui apenas ${anoA-anoN} anos você está na flor da idade ! `)
--- modulo_1/aula_01/exercicioFix_1.js
+++
@@ -1,4 +0,0 @@
-const prompt=require("prompt-sync")()
-const notaA=+prompt("Digite nota A: ")
-const notaB=+prompt("Digite nota B: ")
-console.log(`\nSua média é ${(notaA*4+notaB*6)/(4+6)}\n`)
