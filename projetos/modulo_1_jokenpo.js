37d7f80889850d238394b6a225fdad965e0c6f36
Dener Gomes Batista <100161372+denerbatista@users.noreply.github.com>
2022-03-19 22:10:22 -0300

    Update modulo_1_jokenpo.js
    Modificado alguns termos nas frases;
    Implementado a opção de cancelar rodadas restantes; 
    Mostra na lista a quantidade de rodadas canceladas.

--- projetos/modulo_1_jokenpo.js
+++ projetos/modulo_1_jokenpo.js
@@ -23,6 +23,7 @@ do{
     console.clear();
     console.log(design('Jokempô','Speed',90).blue); //Gera desenho com o nome do jogo com fonte speed na cor azul.
     let rounds=+prompt(`Defina quantos Rodadas deseja jogar ${name}: `); //Variavel que armazena a quantidade de rodadas.
+    var stop; //Variavel de cancelamento de rodadas restantes.
     console.clear();
     while(rounds<=0||isNaN(rounds)){ 
         console.clear();
@@ -38,7 +39,7 @@ do{
     const options=[['PEDRA','PAPEL','TESOURA'],['ESMAGA','EMBRULHA','CORTA']];
 
     //Loop de rodadas de acordo com a quantidade escolhida.
-    for(i=0;i<rounds;i++){
+    for(var i=0;i<rounds;i++){
         //Jogada do computador e armazenamento.
         const computer=Math.floor(Math.random()*3+1); //Jogada do computador.
         choice[0].push(computer-1); //Armazena a jogada do computador convertendo 1,2 e 3 para 0,1 e 2.
@@ -47,16 +48,16 @@ do{
         console.clear();
         console.log(design(`Rodada ${i+1}`,'Standard',90).blue); //Gera desenho da rodada com a contagem atual dela.
         console.log(`Digite o número de sua escolha ${name}: 1-${options[0][0]}, 2-${options[0][1]}, 3-${options[0][2]}`); //Exibe nome do usuário e as opções de jogada. 
-        let user=+prompt('R: '); //jogada do usuário.
+        let user=prompt('R: '); //jogada do usuário.
         choice[1].push(user-1); //Armazena a jogada do usuário convertendo 1,2 e 3 para 0,1 e 2.
 
-        //Verificação de empate, derrota, vitória e opção inválida
+        //Verificação de empate, derrota, vitória e opção inválida ou parar antes de terminar as rodadas.
         if(user==computer){ //Condição de empate.
             console.clear();
             choice[2].push('- EMPATOU'); //Armazena resultado de empate na rodada.
             console.log(design(`${options[0][user-1]}  X  ${options[0][computer-1]}`,'Standard',105).yellow); //Gera Desenho com as jogadas do usuário e computador na cor amarelo.
             console.log(`Você e o computador escolheram ${options[0][user-1]} - Empate ninguém marcou ponto na rodada ${i+1} - Placar: Computador ${score[0]} X ${score[1]} ${name}\n`)
-            prompt(`Restam ${rounds-i-1} rodadas digite qualquer tecla para continuar: `);
+            stop=prompt(`Restam ${rounds-i-1} rodadas digite "pare" para finalizar agora ou enter para continuar: `);
             console.clear();
         }else if((user==1&&computer==2)||(user==2&&computer==3)||(user==3&&computer==1)){ //Todas as condições de derrota do usuário.
             console.clear();
@@ -64,7 +65,7 @@ do{
             choice[2].push('- Computador GANHOU'); //Armazena resultado de ganho do computador na rodada.
             console.log(design(`${options[0][user-1]}  X  ${options[0][computer-1]}`,'Standard',105).red); //Gera Desenho com as jogadas do usuário e computador na cor vermeho.
             console.log(`"${options[0][computer-1]} ${options[1][computer-1]} ${options[0][user-1]}" - Você perdeu a rodada ${i+1} - Placar: Computador ${score[0]} X ${score[1]} ${name}\n`); // Forma frase de ação da jogada de acordo com a escolha do computador e usuário, informa ao usuario que que ele perdeu a rodada e mostra o placar parcial. 
-            prompt(`Restam ${rounds-i-1} rodadas digite qualquer tecla para continuar: `);
+            stop=prompt(`Restam ${rounds-i-1} rodadas digite "pare" para finalizar agora ou enter para continuar: `);
             console.clear();
         }else if((user==2&&computer==1)||(user==1&&computer==3||(user==3&&computer==2))){ //Todas as condições de vitória do usuário.
             console.clear();
@@ -72,16 +73,19 @@ do{
             choice[2].push(`- ${name} GANHOU`); //Armazena resultado de ganho do usuário na rodada.
             console.log(design(`${options[0][user-1]}  X  ${options[0][computer-1]}`,'Standard',105).green); //Gera Desenho com as jogadas do usuário e computador na cor verde.
             console.log(`"${options[0][user-1]} ${options[1][user-1]} ${options[0][computer-1]}" - Você ganhou a rodada ${i+1} - Placar: Computador ${score[0]} X ${score[1]} ${name}\n`); // Forma frase de ação da jogada de acordo com a escolha do computador e usuário, informa ao usuario que que ele ganhou a rodada e mostra o placar parcial. 
-            prompt(`Restam ${rounds-i-1} rodadas digite qualquer tecla para continuar: `);
+            stop=prompt(`Restam ${rounds-i-1} rodadas digite "pare" para finalizar agora ou enter para continuar: `);
             console.clear();
         }else{ //Condição para escolha invalida do usuário.
             console.clear();
             choice[0].length=i; //Apaga jogada do computador armazenada
-            choice[1].length=i;//Apaga jogada do computador armazenada
+            choice[1].length=i;//Apaga jogada inválida do usuário armazenada
             i--; //Anula a rodada.
             console.log(design('ERRO','ghost',90).red); //Gera desenho com palavra erro na cor vermelha com tema fantasma.
-            console.log(`${user} é uma alternativa invalida escolha 1, 2 ou 3\n`); //Retorna a resposta do usuário e informa quais as opções possiveis.
-            prompt('Digite qualquer tecla para tentar novamente: ');
+            console.log(`Sua escolha ${user} é uma alternativa invalida escolha 1, 2 ou 3\n`); //Retorna a resposta do usuário e informa quais as opções possiveis.
+            prompt('Tecle ENTER para tentar novamente: ');
+        }
+        if(stop.toLocaleLowerCase()=="pare"){ //Condição para anular o restande das rodadas.
+            break;
         }
     } 
     
@@ -99,13 +103,16 @@ do{
     }
 
     //Pergunta ao usúario se deseja reniciar e/ou verificar lista de rodadas.
-    var loop=prompt("Digite 'R' para reiniciar, 'L' para lista de jogadas ou qualquer tecla para terminar: ").toLowerCase(); //Variavel de escolha de reiniciar ou verificar rodadas.
+    var loop=prompt("Digite 'R' para jogar novamente, 'L' para lista de jogadas ou qualquer tecla para terminar: ").toLowerCase(); //Variavel de escolha de reiniciar ou verificar rodadas.
     console.clear();
     if(loop=='l'){ //Condição para verificar a lista de rodadas.
         console.log(design('Jokempô','Speed',90).blue); //Gera desenho com o nome do jogo com fonte speed na cor azul.
-        for(i in choice[0]){
-        console.log(`\nRodada ${Number(i)+1} - Computador ${options[0][choice[0][i]]} X ${options[0][choice[1][i]]} ${name} ${choice[2][i]}\n`); //Gera resumo de todas as rodadas jogadas.     
+        for(j in choice[0]){
+            console.log(`\nRodada ${Number(j)+1} - Computador ${options[0][choice[0][j]]} X ${options[0][choice[1][j]]} ${name} ${choice[2][j]}\n`); //Gera resumo de todas as rodadas jogadas.     
         }
-        loop=prompt("Digite 'R' para reiniciar ou qualquer tecla para terminar: ").toLowerCase(); //Depois de mostrar a lista de rodadas pergunta novamente se deseja reiniciar.
+        if(stop=="pare"){
+            console.log(`${name} CANCELOU ${rounds-i-1} RODADAS RESTANTES.\n`) // Demonstra quantas rodadas o usuário cancelou.
+        }
+        loop=prompt("Digite 'R' para jogar novamente ou qualquer tecla para terminar: ").toLowerCase(); //Depois de mostrar a lista de rodadas pergunta novamente se deseja reiniciar.
     }      
 }while(loop=='r');
